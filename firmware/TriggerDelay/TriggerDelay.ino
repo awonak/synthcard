0adc30fcb10c20cbe5900bdafff6c934eed5bd1b
Adam Wonak <adam.wonak@gmail.com>
2024-02-17 11:48:39 -0600

    improve variable names

--- firmware/TriggerDelay/TriggerDelay.ino
+++ firmware/TriggerDelay/TriggerDelay.ino
@@ -23,8 +23,6 @@ const uint8_t LED_PIN[SLIDER_COUNT] = {LED1, LED2, LED3, LED4};
 const float VREF = 4.6 / 1023.0;
 
 // Global state variables.
-int ain = 0;  // Raw cv read from pot for current step
-int old_ain = 0;
 uint8_t din = 0;  // Digital input read value.
 uint8_t old_din = 0;
 int slider[SLIDER_COUNT];  // Hold the current slider value in an array of
@@ -48,7 +46,7 @@ int attack_slope;
 int release_slope;
 int attack_count;
 int release_count;
-int i;
+int counter;
 int output;
 
 void setup() {
@@ -74,9 +72,7 @@ void setup() {
 
 void loop() {
     // Read all inputs.
-    old_ain = ain;
     old_din = din;
-    ain = analogRead(ANALOG_IN);
     din = digitalRead(DIGITAL_IN);
     for (int i = 0; i < SLIDER_COUNT; i++) {
         slider[i] = analogRead(SLIDER_PIN[i]);
@@ -90,19 +86,19 @@ void loop() {
     // Detect if a new trigger has been received.
     if (old_din == 0 && din == 1) {
         // Read the trigger delay and duration.
-        trig_delay = map(analogRead(P1), 0, 1023, 0, 2000);
-        trig_duration = map(analogRead(P3), 0, 1023, 20, 2000);
+        trig_delay = map(slider[0], 0, 1023, 0, 2000);
+        trig_duration = map(slider[2], 0, 1023, 20, 2000);
         trig_start = millis();
 
         // Calculate the exponential slope value.
-        attack_count = analogRead(P2);
-        release_count = analogRead(P4);
+        attack_count = slider[1];
+        release_count = slider[3];
         attack_slope = (attack_count * log10(2)) / log10(255);
         release_slope = (release_count * log10(2)) / log10(255);
 
         stage = DELAY;
         output = 0;
-        i = 1;
+        counter = 1;
     }
 
     switch (stage) {
@@ -112,28 +108,28 @@ void loop() {
             }
             break;
         case ATTACK:
-            if (i >= attack_count) {
+            if (counter >= attack_count) {
                 stage = GATE;
                 trig_start = millis();
-                i = release_count;
-                output = 255;
+                counter = release_count;
             } else {
-                output = min(pow(2, (float(i) / float(attack_slope))), 255);
-                i = min(i++, attack_count);
+                output = min(pow(2, (float(counter) / float(attack_slope))), 255);
+                counter = min(counter++, attack_count);
             }
             break;
         case GATE:
+            output = 255;
             if (millis() > trig_start + trig_duration) {
                 stage = RELEASE;
             }
             break;
         case RELEASE:
-            if (i == 0) {
+            if (counter == 0) {
                 stage = WAIT;
                 output = 0;
             } else {
-                output = min(pow(2, float(i) / float(release_slope)), 255);
-                if (i > 0) i--;
+                output = min(pow(2, float(counter) / float(release_slope)), 255);
+                if (counter > 0) counter--;
             }
             break;
         case WAIT:
